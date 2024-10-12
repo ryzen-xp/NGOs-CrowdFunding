258f916c37a4de8f64486b1795fdcad158ae2b19
Sandeep Chauhan <iron4540@gmail.com>
2024-10-12 23:15:43 +0530

    add

--- src/NGO.sol
+++ src/NGO.sol
@@ -4,41 +4,62 @@ pragma solidity ^0.8.27;
 contract NGO_Funding {
   
     address public admin;
+    uint256 public  idx;
 
     // Struct to define NGO properties
     struct NGO {
-        string uri;            
-        address owner;          
+        string uri; 
+        address owner;    
+        uint  totalDonor ;       
+             
         uint256 totalValue;    
         bool isRegistered;        
         bool blacklisted;      
-        mapping(address => bool) donor;
+       
+    }
+    struct Donor{
+        
+        uint totalDonation;
+        
+
     }
 
    
-    mapping(address => NGO) public NGOs;
+    mapping(uint256 => NGO) public NGOs;
+     mapping(address => bool) donor;
 
     // Constructor sets the admin
-    constructor() {
+    constructor(uint _count) {
         admin = msg.sender;
+        idx = _count ;
     }
 
     // Function to register an NGO
     function Register(string calldata _uri) external {
         // Ensure the NGO is not already registered
-        require(!NGOs[msg.sender].isRegistered, "NGO already registered");
+        
+        // require(!NGOs[i].isRegistered, "NGO already registered");
 
         // Initialize the NGO (only non-mapping fields)
-        NGO storage newNGO = NGOs[msg.sender]; // Get a reference to the NGO struct
+        NGO storage newNGO = NGOs[idx]; // Get a reference to the NGO struct
         newNGO.uri = _uri;
-        newNGO.owner = msg.sender;
+        newNGO.owner = address(msg.sender);
+        newNGO.totalDonor = 0;
+       
         newNGO.totalValue = 0;
         newNGO.isRegistered = true;
         newNGO.blacklisted = false;
+        ++idx;
     }
 
 
-    fucntion Donate() external payable {
-        
+    function donate(uint _idx) external payable {
+        require(NGOs[_idx].isRegistered , "Idx is not registered");
+        require(msg.value > 0 , "Invalid value");
+        NGO storage ngo = NGOs[_idx];
+        ngo.totalValue +=msg.value;
+        ngo.totalDonor ++;
+
+
     }
 }
